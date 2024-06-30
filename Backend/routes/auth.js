70d9d54f3b02fb512ee1ea8f26dc2a7ac8d73b27
atharvTiwalkar1 <atharv.22210869@viit.ac.in>
2024-07-01 03:51:47 +0530

    inotebook continue-3

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,20 +1,43 @@
 const express=require('express');
 const router=express.Router();
 const User=require('../models/User')
+const {body,validationResult}= require("express-validator"); 
 
 
 
 //Creat user using: POST "/api/auth/". Doesn't require Auth 
-router.post('/',(req,res)=>{//try to use post get is not secure
+
+router.post('/',[//try to use post get is not secure
+    body('name').isLength({min:5}),
+    body('email').isEmail(),
+    body('password').isLength({min:5})],(req,res)=>{
+    const errors=validationResult(req);
+
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors:errors.array()});
+    }
+
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      })
+        .then((user) => res.json(user))
+        .catch((err) => {
+          console.log(err);
+          res.json({ error: "Enter a unique value", message: err.message });
+    });
+    
+    // const user= new  User(req.body);
     // obj={    
     //     name:"atharv"
     // }
     // res.json(obj);
-    console.log(req.body);
-    const user =User(req.body);
-    user.save();
+    // console.log(req.body);
+    // const user =User(req.body);
+    // user.save();
     // res.send("hello")
-    res.send(req.body)
+    // res.send(req.body)
     
     // res.json([]);
 })
