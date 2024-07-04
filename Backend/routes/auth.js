8be4ade464e2e1c8cc9d5b11ac0d73f979ef69d7
atharvTiwalkar1 <atharv.22210869@viit.ac.in>
2024-07-05 01:44:37 +0530

    inotebook continue

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -4,10 +4,10 @@ const jwt = require('jsonwebtoken');//for jwt token
 const router=express.Router();
 const User=require('../models/User')
 const {body,validationResult}= require("express-validator"); 
-
+const fetchuser=require('../middleware/fetchuser')
 const JWT_SECRET ="Atharv-secret"
 
-//Creat user using: POST "/api/auth/createuser". No login require
+//ROUTE 1: Create user using: POST "/api/auth/createuser". No login require
 
 router.post('/createuser',[//try to use post get is not secure
     body('name').isLength({min:5}),
@@ -23,7 +23,7 @@ router.post('/createuser',[//try to use post get is not secure
     }
 
     //Check for the duplicate email
-    //always use try catch we never what can happen 
+    //always use try catch we never know what can happen 
     try{
     let user=await User.findOne({email:req.body.email});
     
@@ -51,7 +51,7 @@ router.post('/createuser',[//try to use post get is not secure
 
     }catch(error){
       console.error(error.message);
-      res.status(500).json({error:"Some error occured "})
+      res.status(500).send({error:"Some error occured "})
     }
   
     //     .then((user) => res.json(user))
@@ -73,6 +73,65 @@ router.post('/createuser',[//try to use post get is not secure
     
     // res.json([]);
     
+
+
 })
+
+//ROUTE 2: Authenticate user using : POST "/api/auth/login" . No login required 
+router.post('/login',
+
+  [
+    body('email').isEmail(),
+    body('password').exists()
+  ],
+
+  async(req,res)=>{
+
+    const errors=validationResult(req);
+
+    //If there are errors return bad requests 
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors:errors.array()});
+    }
+
+    const {email,password}= req.body;
+    try{
+      const user=await User.findOne({email});
+      if(!user){
+        return res.status(400).json({error:"Please enter valid credential"})//always try to hide what happened in backed work 
+      }
+      const passwordCompare= await bcrypt.compare(password,user.password);
+      if(!passwordCompare){
+        return res.status(400).json({error:"Please enter valid credential"})
+      }
+      const data={
+        user:{
+          id:user.id
+        }
+      }
+      const authtoken=jwt.sign(data,JWT_SECRET);
+      res.json({authtoken:authtoken})
+    
+    }catch(error){
+      console.error(error.message)
+      res.status(500).send({error:"Internal server error "})
+    }
+  }
+  )
+
+//ROUTE 3:Get logged in user detail using : POST "/api/auth/getuser". Login required 
+
+router.post('/getuser',fetchuser,
+  async(req,res)=>{
+    try{
+      userId=req.user.id;
+      const user=await User.findById(userId).select("-password");
+      res.send(user)
+    }catch(error){
+      console.error(error.message);
+      res.status(500).send("Internal server error")
+    }
+  }
+)
 
 module.exports=router
